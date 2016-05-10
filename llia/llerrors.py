b2c46e2e91e5a2bfedce59fd516835733821d749
Steven Jones <plewto@gmail.com>
2016-05-10 15:22:08 -0500

    Adds LliaError and LliaPingError classes.
    Moved contents of llscript/lserrors to llerrors.
    Made LliacriptError a subclass of LliaError.

--- llia/llerrors.py
+++ llia/llerrors.py
@@ -1,7 +1,17 @@
-# llia.llscript.lserrors
-# 2016.05.09
+# llia.llerrors
+# 2016.05.10
 
-class LliascriptError(Exception):
+class LliaError(Exception):
+
+    def __init__(self, msg=""):
+        super(LliaError, self).__init__(msg)
+
+class LliaPingError(LliaError):
+
+    def __init__(self, msg=""):
+        super(LliaPingError, self).__init__(msg)
+
+class LliascriptError(LliaError):
 
     def __init__(self, msg=""):
         super(LliascriptError, self).__init__(msg)
@@ -15,4 +25,4 @@ class NoSuchBusError(LliascriptError):
 
     def __init__(self, msg=""):
         super(NoSuchBusError, self).__init__(msg)
-    
+                        
