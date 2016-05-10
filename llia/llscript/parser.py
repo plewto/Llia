9f9526f387a644df733e816f6f4b5111b91e2fc6
Steven Jones <plewto@gmail.com>
2016-05-10 03:28:56 -0500

    Code cleanup

--- llia/llscript/parser.py
+++ llia/llscript/parser.py
@@ -23,7 +23,6 @@ class LSLParser(object):
         self._init_dispatch_table()
         self.prompt = "Llia> "
         self._current_line = ""
-        # self.current_synth = None  # String stype_id
         self.buffer_helper = None
         self.synth_helper = None
 
@@ -37,7 +36,6 @@ class LSLParser(object):
         self.dispatch_table["cbus"] = self.add_control_bus
         self.dispatch_table["clear-history"] = self.clear_history
         self.dispatch_table["dump"] = self.dump
-        #self.dispatch_table["efx"] = self.add_efx
         self.dispatch_table["exit"] = self.exit_ 
         self.dispatch_table["x"] = self.exit_ # ISSUE: Remove after testing
         self.dispatch_table["free"] = self.free
@@ -46,16 +44,11 @@ class LSLParser(object):
         self.dispatch_table["ls"] = self.list_
         self.dispatch_table["panic"] = self.panic
         self.dispatch_table["ping"] = self.ping
-        # self.dispatch_table["post"] = self.post   # ISSUE: Not well implemented
-        # self.dispatch_table["postln"] = self.postln
         self.dispatch_table["python"] = self.exec_python
         self.dispatch_table["run"] = self.run_script
         self.dispatch_table["sync"] = self.sync_all
-        #self.dispatch_table["synth"] = self.add_synth
-        #self.dispatch_table["with-synth"] = self.with_synth
 
     def update_prompt(self):
-        #cs = str(self.current_synth)
         cs = str(self.synth_helper.current_synth)
         cb = str(self.buffer_helper.current_buffer)
         self.prompt = "Lila(synth: %s, buffer: %s)> " % (cs, cb)
@@ -184,7 +177,6 @@ class LSLParser(object):
                 self.warning(msg)
                 return False
 
-
     def sync_all(self, tokens):
         self.app.sync_all()
         return True
@@ -264,15 +256,10 @@ class LSLParser(object):
         else:
             ls_targets = ("abus", "cbus", "buffers", "synths", "efx", "command",
                           "syhth-types", "efx-types", "keymodes")
-            
-            #msg = "Expected one of 'abus', 'cbus', 'buffers', 'synths', 'efx', or 'command', "
-            #msg = msg + "not %s" % target
-            #self.warning(msg)
             self.warning("Expected one of the following ls targets, not '%s'" % target)
             for t in sorted(ls_targets):
                 print("   %s" % t)
             return False
-
     
     # abus name [channels]
     #
@@ -339,102 +326,6 @@ class LSLParser(object):
         filename = parse_positional_args(tokens, ["str", "str"])[1]
         rs = self.exec_scriptfile(filename)
         return rs
-
-
-    # # synth stype id_ [:keymode km][:voice-count vc]
-    # #                 [:outbus busName][:outbus-offset n][:outbus-param param]
-    # def add_synth(self, tokens):
-    #     args = parse_keyword_args(tokens,
-    #                                          ["str", "str", "int"],
-    #                                          [":keymode", ":voice-count",
-    #                                           ":outbus", ":outbus-offset",
-    #                                           ":outbus-param"],
-    #                                          {":keymode" : ["str", "Poly1"],
-    #                                           ":voice-count" : ["int", 8],
-    #                                           ":outbus" : ["str", "out_0"],
-    #                                           ":outbus-offset" : ["int", 0],
-    #                                           ":outbus-param" : ["str", "outbus"]})
-    #     cmd, stype, id_, keymode, voice_count, obusName, obusOffset, obusParam = args
-    #     sid = "%s_%d" % (stype, id_)
-    #     if stype not in con.SYNTH_TYPES:
-    #         msg = "Unknown synth type: '%s'" % stype
-    #         self.warning(msg)
-    #         return False
-    #     if keymode not in con.KEY_MODES:
-    #         msg = "Invalid keymode: '%s'" % keymode
-    #         self.warning(msg)
-    #         return False
-    #     if self.proxy.synth_exists(stype, id_):
-    #         msg = "Synth %s already exists" % sid
-    #         self.warning(msg)
-    #         return False
-    #     if not self.proxy.audio_bus_exists(obusName):
-    #         msg = "Audio bus '%s' does not exists" % obusName
-    #         self.warning(msg)
-    #         return False
-    #     rs = self.proxy.add_synth(stype, id_, keymode, voice_count)
-    #     if rs:
-    #         self.proxy.assign_synth_audio_bus(stype, id_, obusParam, obusName, obusOffset)
-    #         self.current_synth = sid
-    #         self.update_prompt()
-    #     return rs
-
-    
-    # # efx stype id_ inbus [:outbus name][:outbus-offset n][:outbus-param p]
-    # #                     [:inbus-offset n][:inbus-param p]
-    # def add_efx(self, tokens):
-    #     req_args = ["str", "str", "int", "str"]
-    #     key_arg_order =  [":outbus", ":outbus-offset",":outbus-param", 
-    #                       ":inbus-offset",":inbus-param"]
-    #     key_args = {":outbus" : ["str", "out_0"],
-    #                 ":outbus-offset" : ["int", 0],
-    #                 ":outbus-param" : ["str", "outbus"],
-    #                 ":inbus-offset" : ["int", 0],
-    #                 ":inbus-param" : ["str", "inbus"]}
-    #     args = parse_keyword_args(tokens,
-    #                                          req_args,
-    #                                          key_arg_order,
-    #                                          key_args)
-    #     cmd, stype, id_, ibs, obs, oboff, obprm, iboff, ibprm = args
-    #     sid = "%s_%d" % (stype, id_)
-    #     if stype not in con.EFFECT_TYPES:
-    #         msg = "Unknown EFX synth type: '%s'" % stype
-    #         self.warning(msg)
-    #         return False
-    #     if self.proxy.synth_exists(stype, id_):
-    #         msg = "Synth %s already exists" % sid
-    #         self.warning(msg)
-    #         return False
-    #     for bs in (obs, ibs):
-    #         if not self.proxy.audio_bus_exists(bs):
-    #             msg = "Audio bus '%s' does not exists" % bs
-    #             self.warning(msg)
-    #             return False
-    #     rs = self.proxy.add_efx(stype, id_)
-    #     if rs:
-    #         self.proxy.assign_synth_audio_bus(stype, id_, obprm, obs, oboff)
-    #         self.proxy.assign_synth_audio_bus(stype, id_, ibprm, ibs, iboff)
-    #         self.current_synth = sid
-    #         self.update_prompt()
-    #     return rs
-    
-    # with stype id_
-    # Select synth for editing.
-    #
-    # def with_synth(self, tokens):
-    #     args = parse_positional_args(tokens,
-    #                                            ["str", "str", "int"])
-    #     cmd, stype, id_ = args
-    #     sid = "%s_%d" % (stype, id_)
-    #     if self.proxy.synth_exists(stype, id_):
-    #         msg = "Using synth '%s'" % sid
-    #         self.status(msg)
-    #         self.update_prompt()
-    #         return True
-    #     else:
-    #         msg = "Synth '%s' does not exists" % sid
-    #         self.warning(msg)
-    #         return False
         
     def print_history(self, tokens):
         print("History")
