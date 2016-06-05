bb12168599104ea5cb0c5b64f7af2589a72920cf
Steven Jones <plewto@gmail.com>
2016-06-05 01:27:44 -0500

    Reformated tk channel and contrller name dialogs

---
+++ llia/gui/tk/tk_bankeditor.py
@@ -0,0 +1,14 @@
+# llia.gui.tk.tk_bankeditor
+# 2016.06.05
+
+from __future__ import print_function
+
+from Tkinter import (BOTH, )
+from ttk import Frame
+
+import llia.gui.tk.tk_factory as factory
+
+
+class TkBankEditor(Frame):
+
+    
