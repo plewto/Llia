820b2cb6029ac5dea8d6d3c6f178e14538ce804c
Steven Jones <plewto@gmail.com>
2016-06-19 11:43:07 -0500

    Bank editor mostly implemented.
    Still need to integrate GUI sync functions.

--- llia/gui/tk/tk_bankeditor.py
+++ llia/gui/tk/tk_bankeditor.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 import os.path
 
-from Tkinter import (Toplevel, Frame)
+from Tkinter import (Toplevel, Frame, StringVar)
 import tkFileDialog
 
 import llia.gui.tk.tk_factory as factory
@@ -125,7 +125,7 @@ class TkBankEditor(Frame):
             name = prg.name
             self.listbox.insert("end", "[%03d] %s" % (slot, name))
         slot = bnk.current_slot
-        program = bnk[slot]
+        program = bnk[None]
         self.listbox.selection_set(slot)
         self.listbox.see(slot)
         txt = "%03d %-8s" % (slot, program.name[:8])
@@ -222,13 +222,10 @@ class TkBankEditor(Frame):
         lab_name = factory.label(frame, "Bank name :  '%s'" % name)
         lab_filename = factory.label(frame, "Filename :  '%s'" % filename)
         frame_text = factory.label_frame(frame, "Remarks")
-
         lab_title.grid(row=0, column=0, columnspan=3,pady=12)
         lab_name.grid(row=1, column=0, sticky="w", padx=4)
         lab_filename.grid(row=2, column=0, sticky="w", padx=4)
-
         frame_text.grid(row=3, column=0, columnspan=3, padx=4, pady=8)
-
         text_widget = factory.text_widget(frame_text, "Bank remarks")
         text_widget.insert('end', remarks)
         text_widget.config(width=80, height=20)
@@ -237,7 +234,6 @@ class TkBankEditor(Frame):
         text_widget.grid(row=0, column=0, rowspan=8, columnspan=8, ipadx=4, ipady=4)
         vsb.grid(row=0, column=8, rowspan=8, columnspan=1, sticky="ns")
         hsb.grid(row=8, column=0, rowspan=1, columnspan=8, sticky="ew")
-
         vsb.config(command=text_widget.yview)
         hsb.config(command=text_widget.xview)
         text_widget.config(yscrollcommand=vsb.set)
@@ -272,25 +268,182 @@ class TkBankEditor(Frame):
         self.app.main_window().display_help(HELP_TOPIC)
 
     def _copy_performance(self):
-        print("copy p")
+        bnk = self.synth.bank()
+        bnk.copy_performance()
+        self.status("Performance copied to clipboard")
 
     def _paste_performance(self):
-        print("paste p")
+        try:
+            bnk = self.synth.bank()
+            bnk.paste_performance()
+            self.status("Clipboard pasted to performance")
+        except KeyError:
+            msg = "Performance clipboard empty"
+            self.warning(msg)
 
     def _fill_performance(self):
-        print("fill p")
+        bnk = self.synth.bank()
+        dialog = Toplevel(self)
+        frame = factory.frame(dialog)
+        frame.pack(expand=True, fill="both")
+        lab_title = factory.label(frame, "Fill Performance")
+        lab_start = factory.label(frame, "Starting slot")
+        lab_end = factory.label(frame, "Ending slot")
+        var_start = StringVar()
+        var_end = StringVar()
+        spin_start = factory.int_spinbox(frame, var_start, 0, len(bnk))
+        spin_end = factory.int_spinbox(frame, var_end, 0, len(bnk))
+        var_start.set(bnk.current_slot)
+        var_end.set(bnk.current_slot+1)
+        lab_title.grid(row=0, column=0, columnspan=3, padx=4, pady=8)
+        lab_start.grid(row=1, column=0, sticky="w", padx=4)
+        spin_start.grid(row=1, column=1, columnspan=2, padx=4)
+        lab_end.grid(row=2, column=0, sticky="w", padx=4)
+        spin_end.grid(row=2, column=1, columnspan=2, padx=4)
+
+        def accept():
+            try:
+                a,b = int(var_start.get()), int(var_end.get())
+                bnk.fill_performance(a,b)
+                msg = "Performance filled slots [%3d, %3d]" % (a,b)
+                self.status(msg)
+                dialog.destroy()
+            except ValueError:
+                msg = "Invalid slot number"
+                self.warning(msg)
+
+        def cancel():
+            msg = "Performance fill canceld"
+            self.status(msg)
+            dialog.destroy()
+            
+        b_accept = factory.accept_button(frame, command=accept)
+        b_cancel = factory.cancel_button(frame, command=cancel)
+        b_accept.grid(row=3, column=1, padx=4, pady=8,sticky="ew")
+        b_cancel.grid(row=3, column=2, padx=4, pady=8,sticky="ew")
+        dialog.grab_set()
+        dialog.mainloop()
 
     def _store_program(self):
-        print("store")
+        bnk = self.synth.bank()
+        program = bnk[None]
+        slot = bnk.current_slot
+        var_name = StringVar()
+        var_start = StringVar()
+        var_end = StringVar()
+        var_name.set(program.name)
+        var_start.set(slot)
+        var_end.set(slot+1)
+        dialog = Toplevel()
+        frame = factory.frame(dialog)
+        frame.pack(expand=True, fill="both")
+        lab_title = factory.label(frame, "Store Program")
+        lab_name = factory.label(frame, "Name")
+        lab_start = factory.label(frame, "Slot")
+        lab_end = factory.label(frame, "End")
+        entry_name = factory.entry(frame, var_name, "Program name")
+        spin_start = factory.int_spinbox(frame, var_start, 0, len(bnk), "Startng slot")
+        spin_end = factory.int_spinbox(frame, var_end, 0, len(bnk), "Ending slot")
+        frame_remarks = factory.label_frame(frame, "Remarks")
+        text_remarks = factory.text_widget(frame_remarks, "Remarks text")
+        text_remarks.insert("end", program.remarks)
+        vsb = factory.scrollbar(frame_remarks, orientation="vertical")
+        hsb = factory.scrollbar(frame_remarks, orientation="horizontal")
+        text_remarks.config(width=40, height=10)
+        text_remarks.grid(row=0, column=0, rowspan=4, columnspan=4)
+        vsb.grid(row=0, column=4, rowspan=4, sticky="ns")
+        hsb.grid(row=4, column=0, columnspan=4, sticky="ew")
+        vsb.config(command=text_remarks.yview)
+        hsb.config(command=text_remarks.xview)
+        text_remarks.config(yscrollcommand=vsb.set)
+        text_remarks.config(xscrollcommand=hsb.set)
+        lab_title.grid(row=0, column=0, columnspan=3, pady=8, padx=4)
+        lab_name.grid(row=1, column=0, padx=4)
+        entry_name.grid(row=1, column=1, columnspan=2)
+        frame_remarks.grid(row=2, column=0, rowspan=4, columnspan=4, padx=4, pady=4)
+        lab_start.grid(row=6, column=0, padx=4, sticky="w")
+        spin_start.grid(row=6, column=1, columnspan=2, pady=4)
+        lab_end.grid(row=7, column=0, padx=4, sticky="w")
+        spin_end.grid(row=7, column=1, columnspan=2)
+        
+        def accept():
+            try:
+                a,b = int(var_start.get()), int(var_end.get())
+                name = var_name.get()
+                remarks = text_remarks.get(1.0, "end")
+                program.name = name
+                program.remarks = remarks
+                for slot in range(a, b):
+                    bnk[slot] = program
+                msg = "Stored program '%s' to slots [%3d, %3d]" % (name, a, b)
+                self.status(msg)
+                dialog.destroy()
+                self.sync()
+            except ValueError:
+                msg = "Invalid slot number"
+                self.warning(msg)
 
+        def cancel():
+            msg = "Store program canceld"
+            self.status(msg)
+            dialog.destroy()
+
+        def auto_increment(*_):
+            try:
+                a = int(var_start.get())
+                var_end.set(a+1)
+            except ValueError:
+                pass
+            
+        b_accept = factory.accept_button(frame, command=accept)
+        b_cancel = factory.cancel_button(frame, command=cancel)
+        b_accept.grid(row=8, column=1, pady=8, sticky="ew")
+        b_cancel.grid(row=8, column=2, pady=8, sticky="ew")
+        spin_start.config(command=auto_increment)
+        dialog.grab_set()
+        dialog.mainloop()
+        
     def _random_program(self):
-        print("random")
+        p = self.synth.random_program()
+        if p:
+            bnk = self.synth.bank()
+            end = len(bnk)-1
+            bnk[end] = p
+            self.synth.use_program(end)
+            self.sync()
+            msg = "Random Program"
+            self.status(msg)
+        else:
+            msg = "Random program function not defined"
+            self.warning(msg)
 
     def _init_program(self):
-        print("init prog")
-
+        flag = init_warning("Initialize Program ?", self.app)
+        if flag:
+            bnk = self.synth.bank()
+            prog = bnk.create_program()
+            bnk.current_program = prog
+            msg = "Program intialized"
+            self.status(msg)
+            self.sync()
+        else:
+            msg = "Program initialize canceld"
+            self.status(msg)
+            
     def _copy_program(self):
-        print("copy prog")
+        bnk = self.synth.bank()
+        bnk.copy_to_clipboard()
+        msg = "Current program copied to clipboard"
+        self.status(msg)
 
     def _paste_program(self):
-        print("paste prog")
+        bnk = self.synth.bank()
+        try:
+            bnk.paste_clipboard()
+            msg = "Clipbaord pasted to current program"
+            self.status(msg)
+            self.sync()
+        except KeyError:
+            msg = "Clipboard does not contain %s program"
+            msg = msg % self.synth.specs["format"]
+            self.warning(msg)
