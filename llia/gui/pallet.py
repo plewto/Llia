651644a3886d97caa53f740a95683b319a7ba97b
Steven Jones <plewto@gmail.com>
2016-06-24 23:42:32 -0500

    Adds hierarchal Pallet

--- llia/gui/pallet.py
+++ llia/gui/pallet.py
@@ -7,44 +7,42 @@ import random
 
 class Pallet(dict):
 
-    def __init__(self):
+    def __init__(self, parent=None):
         super(dict, self).__init__()
+        self.parent = parent
         self["BG"] = "gray5"
         self["FG"] = "#b5b5b5"
         self["ACTIVE-BG"] = "gray15"
         self["ACTIVE-FG"] = "yellow"
-        
         self["BUTTON-BG"] = "#192633"
         self["BUTTON-FG"] = self["FG"]
-        
         self["TAB-BG"] = self["BG"]
         self["TAB-FG"] = self["BUTTON-FG"]
         self["TAB-SELECTED-BG"] = "gray5"
         self["TAB-SELECTED-FG"] = "yellow"
-
         self["DIALOG-BG"] = "#191919"    # Backgroud color for modal dialogs
         self["DIALOG-FG"] = self["FG"]
-        
         self["RADIO-SELECT"] = self["BG"]
         self["SCROLLBAR-BACKGROUND"] = "#344d66"
         self["SCROLLBAR-TROUGH"] = "#333333"
         self["WARNING-FG"] = "yellow"
-
         self["SLIDER-TROUGH"] = self["BG"]
         self["SLIDER-OUTLINE"] = self["BUTTON-BG"]
-        
 
     def __setitem__(self, key, value):
         key = str(key).upper()
         dict.__setitem__(self, key, value)
         
     def __getitem__(self, name):
+        name = str(name).upper()
         try:
-            name = str(name).upper()
             return dict.__getitem__(self, name)
         except KeyError:
-            msg = "WARNING: invalid color: '%s'" % name
-            print(msg)
-            return "gray64"
-
+            if self.parent:
+                return self.parent[name]
+            else:
+                msg = "WARNING: undefined color: '%s'" % name
+                print(msg)
+                return "gray64"
+    
 pallet = Pallet()
