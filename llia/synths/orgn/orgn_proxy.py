a3abbd218320a125f57e0aba808922d00ca528fa
Steven Jones <plewto@gmail.com>
2016-09-10 02:15:20 -0500

    Replaced Orgn editor

--- llia/synths/orgn/orgn_proxy.py
+++ llia/synths/orgn/orgn_proxy.py
@@ -20,16 +20,19 @@ class OrgnProxy(SynthProxy):
     def create_subeditors(self):
         gui = self.app.config["gui"].upper()
         if gui == "TK":
-            from llia.synths.orgn.tk.orgn_ed import create_editor
+            from llia.synths.orgn.tk.editor import create_editor
             appwin = self.app.main_window()
             parent_editor = appwin[self.sid]
             create_editor(parent_editor)
             return parent_editor
             
 orgn_pallet = Pallet(default_pallet)        
-orgn_pallet["BG"] = "#c3c3c3"
-orgn_pallet["FG"] = "#0D0D0D"
-orgn_pallet["RADIO-SELECT"] = "#9c90a6"
+# orgn_pallet["BG"] = "#c3c3c3"
+# orgn_pallet["FG"] = "#0D0D0D"
+# orgn_pallet["RADIO-SELECT"] = "#9c90a6"
+orgn_pallet["SLIDER-TROUGH"] = "#4d424f"
+orgn_pallet["SLIDER-OUTLINE"] = "#464f42"
+
 
 specs["constructor"] = OrgnProxy
 specs["description"] = "Simple FM Synth"
---
+++ llia/synths/orgn/tk/editor.py
@@ -0,0 +1,109 @@
+# llia.synths.orgn.tk.editor
+
+
+from __future__ import print_function
+from Tkinter import Frame
+
+from llia.gui.tk.tk_subeditor import TkSubEditor
+import llia.gui.tk.tk_factory as factory
+import llia.gui.tk.control_factory as cf
+from llia.gui.tk.freq_spinner import FrequencySpinnerControl
+
+from llia.gui.tk.expslider import ExpSlider
+
+
+def create_editor(parent):
+    TkOrgnPanel1(parent)
+
+
+class TkOrgnPanel1(TkSubEditor):
+
+    NAME = "Orgn Tone"
+    IMAGE_FILE = "resources/Orgn/editor.png"
+
+    def __init__(self, editor):
+        frame = editor.create_tab(self.NAME)
+        frame.config(background=factory.bg())
+        canvas = factory.canvas(frame, 946,518, self.IMAGE_FILE)
+        canvas.pack()
+        self.canvas = canvas
+        self.editor = editor
+        TkSubEditor.__init__(self, canvas, editor, self.NAME)
+        editor.add_child_editor(self.NAME, self)
+        y0 = 50
+        y1 = y0 + 230
+        xfreq1, yfreq_offset = 50, 0
+        xamp1 = xfreq1+90
+        xfreq2 = xamp1+60
+        xamp2 = xfreq2+90
+        xattack = xamp2+75
+        xdecay = xattack+60
+        xsustain = xdecay+60
+        xrelease = xsustain+60
+        xmod = xrelease+75
+        xexternmod = xmod + 60
+        xexternpitch = xexternmod + 60
+        xchorus = xexternpitch + 75
+        xchorus_delay = xchorus+60
+        xvfreq = xmod
+        xvdelay = xvfreq+60
+        xvdepth = xvdelay+60
+        xamp = xchorus_delay
+        self.spinner("r2", xfreq1, y0+yfreq_offset)
+        self.norm_slider("amp2",xamp1,y0)
+        self.spinner("r4", xfreq2,y0+yfreq_offset)
+        self.norm_slider("amp4",xamp2,y0) 
+        self.env_time_slider("mattack", xattack, y0)
+        self.env_time_slider("mdecay", xdecay, y0)
+        self.env_time_slider("msustain", xsustain, y0)
+        self.env_time_slider("mrelease", xrelease, y0)
+        self.norm_slider("modulationDepth", xmod, y0)
+        self.norm_slider("xToModulationDepth", xexternmod, y0)
+        self.norm_slider("xToPitch", xexternpitch, y0)
+        self.norm_slider("chorus", xchorus, y0)
+        self.linear_slider("chorusDelay",(0,4),xchorus_delay, y0)
+        self.spinner("r1", xfreq1, y1+yfreq_offset)
+        self.volume_slider("amp1", xamp1, y1)
+        self.spinner('r3', xfreq2, y1+yfreq_offset)
+        self.volume_slider("amp3", xamp2, y1)
+        self.env_time_slider("cattack", xattack, y1)
+        self.env_time_slider("cdecay", xdecay, y1)
+        self.env_time_slider("csustain", xsustain, y1)
+        self.env_time_slider("crelease", xrelease, y1)
+        self.linear_slider("vfreq", (0,8), xvfreq, y1)
+        self.linear_slider("vdelay", (0,4), xvdepth, y1)
+        self.norm_slider("vdepth", xvdelay, y1)
+        self.volume_slider("amp", xamp, y1)
+        
+    def spinner(self, param, x, y, from_=0.25, to=32):
+        s = FrequencySpinnerControl(self.canvas,param,self.editor,from_,to)
+        self.add_control(param,s)
+        s.layout(offset=(x,y))
+        return s
+
+    def norm_slider(self, param, x, y, width=14, height=150):
+        s = cf.normalized_slider(self.canvas, param, self.editor)
+        self.add_control(param,s)
+        s.widget().place(x=x,y=y,width=width, height=height)
+        return s
+
+    def linear_slider(self, param, range_, x, y, width=14, height=150):
+        s = cf.linear_slider(self.canvas, param, self.editor,
+                             range_=range_)
+        self.add_control(param, s)
+        s.widget().place(x=x,y=y,width=width, height=height)
+        return s
+
+    def volume_slider(self, param, x, y):
+        s = cf.volume_slider(self.canvas, param, self.editor)
+        self.add_control(param, s)
+        s.widget().place(x=x, y=y, width=14, height=150)
+        return s
+        
+    def env_time_slider(self, param, x, y):
+        s = ExpSlider(self.canvas, param, self.editor,
+                      range_ = 4, degree=2)
+        self.add_control(param, s)
+        s.layout((x,y), width=14, height=150,
+                 checkbutton_offset=None)
+        return s
