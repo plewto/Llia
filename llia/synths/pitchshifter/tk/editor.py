48bdd53c1251ade425b4e4f01d2f16ab21ae0cc4
Steven Jones <plewto@gmail.com>
2017-05-29 20:49:29 -0500

    Makes modifications to Pitch Shifter effect.
    The new regime allows independence of the delay line and is backwards
    compatible with the previous version.

--- llia/synths/pitchshifter/tk/editor.py
+++ llia/synths/pitchshifter/tk/editor.py
@@ -3,10 +3,6 @@
 
 from llia.gui.tk.tk_subeditor import TkSubEditor
 import llia.gui.tk.tk_factory as factory
-import llia.gui.tk.control_factory as cf
-from llia.gui.tk.msb import MSB,ToggleButton
-from llia.gui.tk.tumbler import Tumbler
-
 
 def create_editor(parent):
     TkPitchShifterPanel(parent)
@@ -28,46 +24,23 @@ class TkPitchShifterPanel(TkSubEditor):
         self.editor = editor
         TkSubEditor.__init__(self, canvas, editor, self.NAME)
         editor.add_child_editor(self.NAME, self)
-
-        y,ypan = 50,250
-        x0 = 100
-        x1 = x0+90
-        x2 = x1+60
-        x3 = x2+60
-        x4 = x3+60
-        x5 = x4+90
-        x6 = x5+60
-        x7 = x6+60
-        
-        def norm_slider(param,x,y):
-            s = cf.normalized_slider(canvas,param,editor)
-            self.add_control(param,s)
-            s.widget().place(x=x,y=y,width=14, height=150)
-
-        def bipolar_slider(param,x,y):
-            s = cf.bipolar_slider(canvas,param,editor)
-            self.add_control(param,s)
-            s.widget().place(x=x,y=y,width=14, height=75)
-
-        def amp_slider(param,x,y):
-            s = cf.volume_slider(canvas,param,editor)
-            self.add_control(param,s)
-            s.widget().place(x=x,y=y,width=14,height=150)
-            
-        tratio = Tumbler(canvas,"pitchRatio",editor,
-                         sign=False,digits=4,scale=0.001,
-                         range_=(0,4000))
-        self.add_control("pitchRatio", tratio)
-        tratio.layout((x0,y))
-        norm_slider("pitchDispersion",x1,y)
-        norm_slider("timeDispersion",x2,y)
-        norm_slider("delay",x3,y)
-        norm_slider("feedback",x4,y)
-        amp_slider("dryAmp",x5,y)
-        amp_slider("psAmp",x6,y)
-        amp_slider("delayAmp",x7,y)
-        bipolar_slider("dryPan",x5,ypan)
-        bipolar_slider("psPan",x6,ypan)
-        bipolar_slider("delayPan",x7,ypan)
-        
-            
+        x0 = 75
+        xdelay = x0+220
+        xmixer = xdelay+350
+        y0,y1 = 75,260
+        self.tumbler("pitchRatio",4,0.001,x0,y0,range_=(0,4000))
+        self.norm_slider("pitchDispersion",x0+80,y0)
+        self.norm_slider("timeDispersion",x0+140,y0)
+        self.toggle("delayInSelect",xdelay,y0,off=(0,"P.Shifter"),on=(1,"Drysig"))
+        self.exp_slider("delay",1,xdelay+80,y0)
+        self.exp_slider("delayMod",1,xdelay+140,y0)
+        self.tumbler("lfoFreq",5,0.001,xdelay+110,y1)
+        self.linear_slider("feedback",(-1,1),xdelay+200,y0)
+        self.exp_slider("lowpass",16000,xdelay+260,y0)
+        self.volume_slider("dryAmp",xmixer,y0)
+        self.volume_slider("psAmp",xmixer+60,y0)
+        self.volume_slider("delayAmp",xmixer+120,y0)
+        self.linear_slider("dryPan",(-1,1),xmixer,y1)
+        self.linear_slider("psPan",(-1,1),xmixer+60,y1)
+        self.linear_slider("delayPan",(-1,1),xmixer+120,y1)
+        self.toggle("feedbackDestination",xdelay,y1,off=(0,"P.shifter"),on=(1,"Delay"))
