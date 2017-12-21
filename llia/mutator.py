8e444145028dd324885ee1af7ef9ea9ac7345537
Steven Jones <plewto@gmail.com>
2017-12-21 14:13:21 -0600

    Bugfixes mutator.

--- llia/mutator.py
+++ llia/mutator.py
@@ -8,26 +8,59 @@
 from __future__ import print_function
 from collections import OrderedDict
 
-from llia.util.lmath import coin,approx,clip
+from llia.util.lmath import coin,approx,clip,rnd
+
+
 
 
 class MutationParameter(object):
 
-    def __init__(self, param, probability, range_, max_ratio=0.1):
+
+    def __init__(self, param, probability, range_, max_ratio=0.1, walker=False):
         self.param = param
         self.probability = probability
         self.range_ = range_
-        self.function = approx
         self.max_ratio = max_ratio
+        if walker:
+            self.function = self.walker
+        else:
+            self.function = self.approx
+
+    # def mutate(self, program):
+    #     current = program[self.param]
+    #     new_value = current
+    #     if coin(self.probability):
+    #         mn,mx = self.range_
+    #         new_value = clip(self.function(current, self.max_ratio),mn,mx)
+    #         program[self.param] = new_value
 
     def mutate(self, program):
         current = program[self.param]
         new_value = current
         if coin(self.probability):
             mn,mx = self.range_
-            new_value = clip(self.function(current, self.max_ratio),mn,mx)
+            new_value = clip(self.function(current), mn,mx)
             program[self.param] = new_value
+        return new_value
 
+    def _scale_approx(self, x):
+        return approx(x,self.max_ratio)
+
+    def _additive_approx(self, x):
+        diff = abs(self.range_[1] - self.range_[0])
+        max_value = diff*self.max_ratio
+        return rnd(max_value)
+
+    def approx(self, x):
+        THRESHOLD = 0.001
+        if abs(x) < THRESHOLD:
+            return self._additive_approx(x)
+        else:
+            return self._scale_approx(x)
+        
+    def walker(self, x):
+        return coin(0.5, x-1, x+1)
+        
     def __str__(self):
         frm = "%s prob %s  range %s  max_ratio %s"
         return frm % (self.param,self.probability,self.range_,self.max_ratio)
@@ -38,8 +71,8 @@ class Mutator(object):
     def __init__(self):
         self._params = OrderedDict()
 
-    def define(self, param, prob, range_, max_ratio=0.1):
-        mp = MutationParameter(param,prob,range_,max_ratio)
+    def define(self, param, prob, range_, max_ratio=0.1, walker=False):
+        mp = MutationParameter(param,prob,range_,max_ratio,walker=walker)
         self._params[param] = mp
 
     def keys(self):
--- llia/synths/ss1/ss1_mutation.py
+++ llia/synths/ss1/ss1_mutation.py
@@ -2,9 +2,9 @@
 
 from __future__ import print_function
 
-from llia.mutation import Mutation
+from llia.mutator import Mutator
 
-class SS1Mutation(Mutation):
+class SS1Mutation(Mutator):
 
     def __init__(self):
         super(SS1Mutation,self).__init__()
@@ -14,6 +14,7 @@ class SS1Mutation(Mutation):
         self.define("lfoFreq", 0.10, (0.001,99),0.01)
         self.define("lfoDelay", 0.10, (0,3),0.01)
         self.define("lfoAmp", 0.10,(0,1),0.01)
+        self.define("lfoWave",0.10,(0,2),1.0,walker=True)
         self.define("attack", 0.10,(0,6),0.01)
         self.define("decay", 0.10,(0,6),0.01)
         self.define("sustain", 0.10,(0,1),0.01)
@@ -21,11 +22,13 @@ class SS1Mutation(Mutation):
         self.define("sawMix", 0.10,(0,1),0.01)
         self.define("pulseMix", 0.10,(0,1),0.01)
         self.define("subMix", 0.10,(0,1),0.01)
+        self.define("chorus", 0.10,(0,1),1.0,walker=True)
+        self.define("subOctave", 0.10, (0,1),1.0,walker=True)
         self.define("noiseMix", 0.10,(0,1),0.01)
+        self.define("noiseSelect",0.10,(0,1),0.01,walker=True)
         self.define("wave", 0.10,(0,1),0.01)
         self.define("waveLFO", 0.10,(0,1),0.01)
         self.define("waveEnv", 0.10,(0,1),0.01)
         self.define("filterLFO", 0.10,(0,8000),0.01)
         self.define("filterEnv", 0.10,(-10000,10000),0.01)
         self.define("filterRes", 0.10,(0,1),0.01)
-
